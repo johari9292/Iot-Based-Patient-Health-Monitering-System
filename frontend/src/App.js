6cdceb81bd1e2b6851a521af04be7048b378d93f
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2019-12-07 01:52:15 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     this.state = {
       response: [],
      
-      endpoint: "http://52.22.174.76:8080",
+      endpoint: "http://52.22.174.76:80",
 
 
     }
