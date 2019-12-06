f7ff47be595ff0a4f7c80d9c16d7f08aded904ea
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2019-12-07 02:31:19 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
 //     socket.on("mydata", data => this.setState({
 //       response: data
 //     }))
-axios.get(`https:iotphms.herokuapp.com:80/get` )
+axios.get('https://iotphms.herokuapp.com/getdata' )
     .then(res => {
       const tasks = res.data;
       this.setState({ response:tasks });
