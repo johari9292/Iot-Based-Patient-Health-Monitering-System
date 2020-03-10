c49bbca165ce3b2436b7ba6113c227f5f5a5a8a8
Ishaq Ali <johari9292@gmail.com>
2020-03-10 18:53:35 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
 const cors = require('cors');
-// const http = require("http");
+const dotenv = require("dotenv").config()
+const http = require("http");
 var config = require("./db/config");
 const express = require("express");
 const bodyParser = require("body-parser");
-const PHMSController = require("./api/api") 
+const PHMSController = require("./apis/api")
 const port = process.env.PORT || 80;
-var http = require('http').Server(app);
-   var  io = require('socket.io')(http);
-// const io = socketIo(server);
+
+const socketIo = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 const ObjectId = require("mongodb").ObjectID;
 const MongoClient = require('mongodb').MongoClient;
 const DATABASE_NAME = "test";
-const uri = "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "heroku_1kkl8s2q";
+const MONGO_URI = "mongodb+srv://joharibalti1996:is119821885@cluster0-jjj5l.mongodb.net/test?retryWrites=true&w=majority";
 
-
-
+// const MONGO_URI = "mongodb://joharibalti:is119821885@ds227352.mlab.com:27352/heroku_1kkl8s2q"
 app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const path = require("path")
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "frontend", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+
 var database, collection;
 
 
 
-MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(MONGO_URI , { useNewUrlParser: true }, (error, client) => {
   if (error) {
     throw error;
   }
@@ -37,9 +43,10 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
   collection = database.collection("phms");
   console.log("Connected to `" + DATABASE_NAME + "`!");
 
-
+  let interval
   io.on("connection", socket => {
     console.log("New client connected");
+    
     if (!socket.sentMydata) {
       collection.find({}).sort({ _id: -1 }).limit(1).toArray(function (err, result) {
        if (err) throw err;
@@ -49,6 +56,18 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
       })
       socket.sentMydata = true;
     }
+    if (interval) {
+      clearInterval(interval);
+    }
+    interval = setInterval(() =>{ 
+      collection.find({}).sort({ _id: -1 }).limit(1).toArray(function (err, result) {
+        if (err) throw err;
+
+        socket.emit("FromAPI", (result));
+
+      })
+    }, 10);
+     
     const changeStream = collection.watch()
     changeStream.on('change', function (change) {
       collection.find({}).sort({ _id: -1 }).limit(1).toArray(function (err, result) {
@@ -59,6 +78,7 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
       })
     })
 
+
     socket.on("disconnect", () => {
       console.log("Client disconnected");
     });
@@ -68,12 +88,13 @@ MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
 
 
 
+
 app
   .route('/getdata')
   .get(PHMSController.getphms)
 
 app
-  .route('/getdata/:id')
+  .route('/get/:id')
   .get(PHMSController.getphmsbyid)
 app
   .route('/adddata/temp=/:temp/&heartbeat=/:heartbeat/&bp=/:bp/&/ecg=/:ecg')
@@ -86,11 +107,12 @@ app
   .route('/delete/:id')
   .delete(PHMSController.deletephms) 
 
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
-
-http.listen(process.env.PORT || 3000, function() {
-  // ...
-})
+server.listen(port, function() {
+  console.log('Listening to port:  ' + port);
+});
+// server.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
